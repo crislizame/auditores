01f7eaeae50f34629a99b914c9e6ec07763b8e77
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-01-21 21:33:41 -0500

    ok

--- resources/views/vistas/pages/admin/agenda/imprimirreportes-item.blade.php
+++ resources/views/vistas/pages/admin/agenda/imprimirreportes-item.blade.php
@@ -133,7 +133,7 @@ $pdsdata = (new \App\Pdsperfile())->where('id',$datos->pds_id)->first();
 
                             @for($images as $image)
                             <td class="col">
-                                <img class="img-responsive" src="{{asset('imagen/$image.jpg')}}">
+                                <img class="img-responsive" src="{{url('/')}}imagen/{{$image}}.jpg">
                             </td>
                             @empty
                             @endforelse
