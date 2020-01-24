81d27e3a102828acc49363589cd23c3e2571b4ab
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-01-24 01:06:07 -0500

    ok

--- resources/views/vistas/pages/admin/agenda/reportes-item.blade.php
+++ resources/views/vistas/pages/admin/agenda/reportes-item.blade.php
@@ -173,7 +173,7 @@
                             $valores += $valor;
                             }
                             @endphp
-                            <div class="col-12 mb-2">
+                            <div class="col-12 mb-2 ml-4">
                                 <div class="row">
                                     <div class="col-4">
                                         <b>{{ucfirst($th->nombre_val)}}</b>
@@ -188,7 +188,7 @@
                                         </div>
                                     </div>
                                     <div class="col-2">
-                                        <button data-id="{{$id}}" class="btn btn-sm btn-primary btn-verfotos">Ver Fotos</button>
+                                        <button data-id="{{$id}}" class="btn btn-sm btn-primary btn-verfotos text-center">Ver Fotos</button>
                                     </div>
                                 </div>
                             </div>
