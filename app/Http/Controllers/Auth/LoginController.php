4c517b1307ee1fcf1b8fc7553ca5e54f27d46208
crislizame <boxgamebig@gmail.com>
2020-02-13 16:30:51 -0500

    Nuevos cambios 1 FEb

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Ipsave;
 use App\User;
+
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
