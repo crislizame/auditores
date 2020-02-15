86be6e852c3cb9c7458754bcb4d1db5452633851
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-14 21:12:46 -0500

    ok

--- app/Http/Controllers/Mantenimiento/MantenimientoController.php
+++ app/Http/Controllers/Mantenimiento/MantenimientoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Mantenimiento;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
@@ -36,8 +37,14 @@ class MantenimientoController extends Controller
         $tbody = "";
         foreach ($ordenes as $orden) {
             $estado = ($orden->estado_orden != null ? mb_strimwidth(strtoupper($orden->estado_orden), '0', '15', '...') : 'Sin orden de trabajo');
-            $inicio = ($orden->finicio != null ? mb_strimwidth(strtoupper($orden->finicio), '0', '15', '...') : '------');
-            $fin = ($orden->ffin != null ? mb_strimwidth(strtoupper($orden->ffin), '0', '15', '...') : '------');
+            $rfinicio = ($orden->rfinicio != null ? mb_strimwidth(strtoupper($orden->rfinicio), '0', '15', '...') : '------');
+
+            $tiempo_estimado = '------';
+            if ($orden->finicio != null && $orden->ffin != null) {
+                $finicio = Carbon::parse($orden->finicio);
+                $ffin = Carbon::parse($orden->ffin);
+                $tiempo_estimado = $finicio->diffForHumans($ffin);
+            }
 
             $tbody .= "<tr>
                         <th scope=\"row\"><a href=\"#\">" . 'C-' . str_pad($orden->idorden_requermientos, 7, "0", STR_PAD_LEFT) . "</a></th>
@@ -45,9 +52,8 @@ class MantenimientoController extends Controller
                         <td>" . mb_strimwidth(strtoupper($orden->subarea), '0', '15', '...') . "</td>
                         <td>" . mb_strimwidth(strtoupper($orden->problema), '0', '15', '...') . "</td>
                         <td>" . mb_strimwidth(strtoupper($orden->cliente), '0', '15', '...') . "</td>
-                        <td>" . mb_strimwidth(strtoupper($orden->rfinicio), '0', '15', '...') . "</td>
-                        <td>" . $inicio . "</td>
-                        <td>" . $fin . "</td>
+                        <td>" . $rfinicio . "</td>
+                        <td>" . $tiempo_estimado . "</td>
                         <td>" . $estado . "</td>
                     </tr>";
         }
