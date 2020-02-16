4e5279b7376faddc1912d2cb4bbcc78ff12be11f
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-15 20:47:01 -0500

    ok

--- resources/views/vistas/pages/mantenimiento/problemas.blade.php
+++ resources/views/vistas/pages/mantenimiento/problemas.blade.php
@@ -18,10 +18,10 @@
                         <div class="col-12">
                             <ul class="nav mb-4">
                                 <a href="#" onclick="cargar('loteria')">
-                                    <li class="nav-item @if($cat == "loteria") active @endif">Loteria</li>
+                                    <li class="nav-item @if($cat == " loteria") active @endif">Loteria</li>
                                 </a>
                                 <a href="#" onclick="cargar('proveedores')">
-                                    <li href="#" class="nav-item @if($cat == "proveedores") active @endif">Proveedores</li>
+                                    <li href="#" class="nav-item @if($cat == " proveedores") active @endif">Proveedores</li>
                                 </a>
                             </ul>
                         </div>
@@ -77,7 +77,7 @@
                                     <input type="hidden" name="req_num_orden">
                                 </div>
                             </div>
-    
+
                             <div class="row">
                                 <div class="col-4">
                                     <label>WTC</label>
@@ -89,7 +89,7 @@
                                     <label>Sub Area</label>
                                 </div>
                             </div>
-    
+
                             <div class="row">
                                 <div class="col-4">
                                     <h3 id="req_cliente"></h3>
@@ -101,10 +101,10 @@
                                     <h3 id="req_subarea"></h3>
                                 </div>
                             </div>
-    
+
                         </div>
                         <div class="col-6">
-                            
+
                         </div>
                     </div>
                     <div class="row">
@@ -190,23 +190,40 @@
                         [25, 50, 100, -1],
                         [25, 50, 100, "Todos"]
                     ],
-                    "columns": [
-                        { "width": "10%" },
-                        { "width": "10%" },
-                        { "width": "10%" },
-                        { "width": "15%" },
-                        { "width": "20%" },
-                        { "width": "10%" },
-                        { "width": "10%" },
-                        { "width": "15%" }
+                    "columns": [{
+                            "width": "10%"
+                        },
+                        {
+                            "width": "10%"
+                        },
+                        {
+                            "width": "10%"
+                        },
+                        {
+                            "width": "15%"
+                        },
+                        {
+                            "width": "20%"
+                        },
+                        {
+                            "width": "10%"
+                        },
+                        {
+                            "width": "10%"
+                        },
+                        {
+                            "width": "15%"
+                        }
                     ]
                 });
             });
         }
     });
 
-    function modalAsignarOrdenDeTrabajo(id) {
+    function modalAsignarOrdenDeTrabajo(id, visualId) {
         $('.modal-asignar').modal('show');
+        $('#req_num_orden').html(visualId);
+        $('[name="req_num_orden"]').val(id);
     }
 </script>
 @endsection
