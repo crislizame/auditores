36e8cc9f0eae67afef281b0746bc582fbb5fd535
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-19 01:00:32 -0500

    ok

--- app/Orden_Requerimiento.php
+++ app/Orden_Requerimiento.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Orden_Requerimiento extends Model
 {
     protected $table = 'orden_requermientos';
+    protected $primaryKey = 'idorden_requermientos';
 }
