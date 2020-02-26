2e7effec34e58c03647ccf2857c7fa8df29d332c
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-25 23:04:28 -0500

    ok

--- resources/views/vistas/pages/mantenimiento/problemas.blade.php
+++ resources/views/vistas/pages/mantenimiento/problemas.blade.php
@@ -311,15 +311,15 @@
                             </div>
 
                             <div class="row">
-                                <div class="col-6" id="cc" style="display: none;">
-                                    <label>Cotización <i class="fa fa-upload"></i><a href="#"> Cargar imagen</a></label>
+                                <div class="col-6" id="cc">
+                                    <label>Cotización <i class="fa fa-upload"></i> Cargar imagen</label>
                                     <input type="file" class="form-control-file border" name="ot_ccotizacion">
                                 </div>
                                 <div class="col-6" id="cv" style="display: none;">
                                     <label>Cotización <i class="fa fa-eye"></i><a href="#" id="cvl"> Ver</a></label>
                                 </div>
-                                <div class="col-6" id="gc" style="display: none;">
-                                    <label>Garantía <i class="fa fa-upload"></i><a href="#"> Cargar imagen</a></label>
+                                <div class="col-6" id="gc">
+                                    <label>Garantía <i class="fa fa-upload"></i> Cargar imagen</label>
                                     <input type="file" class="form-control-file border" name="ot_cgarantia">
                                 </div>
                                 <div class="col-6" id="gv" style="display: none;">
