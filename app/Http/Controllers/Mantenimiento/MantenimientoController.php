048de25f4da5805c627920d793025457a58eeb36
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-28 00:01:27 -0500

    ok

--- app/Http/Controllers/Mantenimiento/MantenimientoController.php
+++ app/Http/Controllers/Mantenimiento/MantenimientoController.php
@@ -37,7 +37,9 @@ class MantenimientoController extends Controller
                     'orden_trabajos.finicio',
                     'orden_trabajos.ffin',
                     'orden_trabajos.estado_orden',
-                    'orden_requermientos.solicitado'
+                    'orden_requermientos.solicitado',
+                    'orden_requermientos.enproceso',
+                    'orden_requermientos.finalizado'
                 )
                 ->join('areas', 'orden_requermientos.area_id', 'areas.idareas')
                 ->join('subareas', 'orden_requermientos.subarea_id', 'subareas.idsubareas')
