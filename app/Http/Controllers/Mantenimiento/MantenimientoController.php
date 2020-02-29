3a2b827d0fde8dd0c199dcc5e36620fcbe69be69
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-02-28 23:09:46 -0500

    ok

--- app/Http/Controllers/Mantenimiento/MantenimientoController.php
+++ app/Http/Controllers/Mantenimiento/MantenimientoController.php
@@ -61,7 +61,7 @@ class MantenimientoController extends Controller
                 }
 
                 $tbody .= "<tr>
-                        <th scope=\"row\"><a href=\"#\" onclick=\"modalAsignarOrdenDeTrabajo(" . $orden->idorden_requermientos . ", '" . $id . "', '"+$orden->entidad+"')\">" . $id . "</a></th>
+                        <th scope=\"row\"><a href=\"#\" onclick=\"modalAsignarOrdenDeTrabajo(" . $orden->idorden_requermientos . ", '" . $id . "', '".$orden->entidad."')\">" . $id . "</a></th>
                         <td>" . mb_strimwidth(strtoupper($orden->area), '0', '15', '...') . "</td>
                         <td>" . mb_strimwidth(strtoupper($orden->subarea), '0', '15', '...') . "</td>
                         <td>" . mb_strimwidth(strtoupper($orden->problema), '0', '15', '...') . "</td>
