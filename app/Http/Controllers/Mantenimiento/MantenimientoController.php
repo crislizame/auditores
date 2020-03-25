d8b2eeb28fb4f3d054a90990486dad6974d9269f
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-03-25 04:13:19 -0500

    ok

--- app/Http/Controllers/Mantenimiento/MantenimientoController.php
+++ app/Http/Controllers/Mantenimiento/MantenimientoController.php
@@ -416,7 +416,7 @@ class MantenimientoController extends Controller
                         <td>" . strtoupper($proveedor->direccion) . "</td>
                         <td>" . strtoupper($proveedor->telefono) . "</td>
                         <td>" . strtoupper($proveedor->correo) . "</td>
-                        <td><span class=\"col\">".$porcentaje."%</span><div style=\"width: 80px;height: 80px;background-repeat: no-repeat;background-image:url('".url('/img/cara')."$calificacion.jpg');\"></div></td>
+                        <td><span class=\"col\">".$porcentaje."%</span><img style=\"width: 80px;height: 80px;\" src=\"".url('/img/cara')."$calificacion.jpg\"></td>
                     </tr>";
         }
         return $tbody;
