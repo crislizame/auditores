8653f5d45a8fc56e17dac051fe95cf3036d3291e
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-03-25 04:17:28 -0500

    ok

--- app/Http/Controllers/Mantenimiento/MantenimientoController.php
+++ app/Http/Controllers/Mantenimiento/MantenimientoController.php
@@ -387,12 +387,12 @@ class MantenimientoController extends Controller
 
     public function cargarProveedores(Request $request)
     {
-        $proveedores = DB::table('proveedores')->orderBy('idproveedores', 'asc')->get();
+        $proveedores = DB::table('proveedores')->orderBy('idproveedores', 'asc')->where('nombre', '!=', 'Mantenimiento')->get();
         $tbody = "";
         foreach ($proveedores as $proveedor) {
             $calificacion = Calificacion::join('orden_trabajos', 'calificaciones.id_orden_trabajo', 'orden_trabajos.idorden_trabajos')->where('proveedor_id', $proveedor->idproveedores)->avg('calificacion');
             $porcentaje = 0;
-            switch($calificacion){
+            switch ($calificacion) {
                 case 1:
                     $porcentaje = 0;
                     break;
@@ -409,14 +409,14 @@ class MantenimientoController extends Controller
                     $porcentaje = 100;
                     break;
             }
-    
+
             $tbody .= "<tr>
                         <th scope=\"row\">" . strtoupper($proveedor->nombre) . "</th>
                         <td>" . strtoupper($proveedor->ruc_cedula) . "</td>
                         <td>" . strtoupper($proveedor->direccion) . "</td>
                         <td>" . strtoupper($proveedor->telefono) . "</td>
                         <td>" . strtoupper($proveedor->correo) . "</td>
-                        <td><span class=\"col\">".$porcentaje."%</span><img style=\"width: 80px;height: 80px;\" src=\"".url('/img/cara')."$calificacion.jpg\"></td>
+                        <td><span class=\"col\">" . $porcentaje . "%</span><img style=\"width: 80px;height: 80px;\" src=\"" . url('/img/cara') . "$calificacion.jpg\"></td>
                     </tr>";
         }
         return $tbody;
