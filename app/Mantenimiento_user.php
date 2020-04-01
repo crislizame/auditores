6700c2cfbe07e9a1a22ba0046a4cedec1733e6f5
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-04-01 05:19:18 -0500

    ok

--- app/Mantenimiento_user.php
+++ app/Mantenimiento_user.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Mantenimiento_user extends Model
 {
     protected $primaryKey = 'idmantenimiento_users';
+    protected $fillable = ['cedula','direccion'];
 }
