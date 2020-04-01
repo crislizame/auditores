abe5fd3b0a583780271e015ea2fc34f6c06200a2
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-04-01 05:28:07 -0500

    ok

--- app/Http/Controllers/Admin/PerfilController.php
+++ app/Http/Controllers/Admin/PerfilController.php
@@ -17,34 +17,7 @@ class PerfilController extends Controller
     public function index()
     {
         $user = User::where('id', Auth::user()->id)->leftJoin('mantenimiento_users', 'users.id', 'mantenimiento_users.user_id')->first();
-        $calificacion = Calificacion::where('id_user_calificado', $user->id)->avg('calificacion');
-
-        $cumplimiento_datos = Proveedor::select('problemas.tiempo', 'orden_requermientos.enproceso', 'orden_requermientos.finalizado')
-            ->join('orden_trabajos', 'proveedores.idproveedores', 'orden_trabajos.proveedor_id')
-            ->join('orden_requermientos', 'orden_trabajos.orden_requermiento_id', 'orden_requermientos.idorden_requermientos')
-            ->join('problemas', 'orden_requermientos.problema_id', 'problemas.id')
-            ->where('proveedores.nombre', 'Mantenimiento')
-            ->get();
-
-        $cumplido = 0;
-        $nocumplido = 0;
-
-        foreach ($cumplimiento_datos as $datos) {
-            $diff = new Carbon($datos->enproceso);
-            $diff = $diff->diffInHours($datos->finalizado);
-            switch (($diff > $datos->tiempo) ? 0 : 1) {
-                case 0;
-                    $nocumplido++;
-                    break;
-                case 1;
-                    $cumplido++;
-                    break;
-            }
-        }
-
-        $cumple = $cumplido >= $nocumplido ? '1 (Dentro del rango)' : '0 (Fuera del rango)';
-
-        return view('vistas.pages.admin.perfil.perfil')->with('user', $user)->with('calificacion', $calificacion)->with('cumple', $cumple);
+        return view('vistas.pages.admin.perfil.perfil')->with('user', $user);
     }
 
     public function modificar(Request $request){
@@ -55,6 +28,7 @@ class PerfilController extends Controller
         $user->save();
 
         $flight = Mantenimiento_user::firstOrCreate(
+            ['user_id' => $user->id],
             ['cedula' => $request->cedula],
             ['direccion' => $request->direccion]
         );
