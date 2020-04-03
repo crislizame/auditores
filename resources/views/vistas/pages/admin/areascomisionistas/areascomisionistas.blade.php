74abbe0e71a2f6c50c2455fb36b09833bae1014e
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-04-03 06:01:40 -0500

    ok

--- resources/views/vistas/pages/admin/areascomisionistas/areascomisionistas.blade.php
+++ resources/views/vistas/pages/admin/areascomisionistas/areascomisionistas.blade.php
@@ -8,13 +8,10 @@
                 <span class="titulos text-info bold">Areas</span>
                 <div class="card">
                     <div class="card-body">
-                        <ul class="nav subm flex-column">
-                            @php
-                            $control=1;
-                            @endphp
+                        <ul class="nav subm flex-column" data-toggle="buttons">
                             @foreach($areas as $area)
-                            <li class="nav-item subm-item">
-                                <a id="l" class="nav-link subm-a p-5" href="#" onclick="buscarSubAreas(this)" data="{{ $area->idareas }}">{{ $area->nombre }}</a>
+                            <li class="nav-item subm-item" data-toggle="button" aria-pressed="false">
+                                <a class="nav-link subm-a p-5" href="#" onclick="buscarSubAreas(this)" data="{{ $area->idareas }}">{{ $area->nombre }}</a>
                             </li>
                             @endforeach
                         </ul>
@@ -41,48 +38,12 @@
     </div>
 </div>
 <script>
-    $(document).ready(function(){
-        $('#l').click();
-    });
-
     function buscarSubAreas(item) {
         @php
         $control = 1;
         @endphp
         @foreach($areas as $area)
         $('#l').removeClass('active');
-        @endforeach
-        $(item).addClass('active');
-
-        $.ajax({
-            url: "{{ url('comisionista/subareas') }}",
-            method: "post",
-            data: {
-                '_token': "{{csrf_token()}}",
-                'id': $(item).attr('data')
-            },
-            beforeSend: function() {
-                swal({
-                    title: "Cargando Subareas",
-                    icon: "info",
-                    buttons: false,
-                    timer: 2000,
-                    closeOnClickOutside: false,
-                    closeOnEsc: false
-                });
-            }
-        }).done(function(done) {
-            $('#subareas').html(done);
-            $('#ls-1').click();
-        });
-    }
-
-    buscarProblemas(item){
-        @php
-        $control = 1;
-        @endphp
-        @foreach($areas as $area)
-        $('#l-{{ $control++ }}').removeClass('active');
         @endforeach
         $(item).addClass('active');
 
