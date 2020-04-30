2342187f96b44ea468ae85bffdfe0b82c1370ddc
Neiber Cardenas Digital <rebienkrdns@gmail.com>
2020-04-30 01:20:51 -0500

    ok

--- resources/views/vistas/pages/admin/indicadores/indicadores.blade.php
+++ resources/views/vistas/pages/admin/indicadores/indicadores.blade.php
@@ -1125,15 +1125,42 @@
                 <span class="col pr-4 fechasel titulos w-50 font-weight-bold">Estado</span>
             </div>
             <div class="row">
-                <div class="col-md-6 offset-med-6 text-center">
-                    <div class="row mb-1">
-                        <div class="col-md-6">Solicitado - Proceso</div>
-                        <div class="col-md-6">Proceso - Cerrado</div>
+                <div class="col-md-6 offset-md-3 text-center">
+                    <div class="row mt-2 mb-1">
+                        <div class="col-md-6 text-primary">Solicitado - Proceso</div>
+                        <div class="col-md-6 text-primary">Proceso - Cerrado</div>
                     </div>
-                    <hr class="mb-1">
+                    <hr class="mb-2">
                     <div class="row mb-1">
-                        <div class="col-md-6"><h2>00:00</h2></div>
-                        <div class="col-md-6"><h2>00:00</h2></div>
+
+@php
+$ordenes = (new \App\Orden_Requerimiento())->select('solicitado','enproceso','finalizado')->whereBetween('solicitado', [$datainicio, $datafin]);
+
+if($pds_id != "%"){
+    $ordenes = $ordenes->where("pds_id",$pds_id);
+} else {
+    
+    if($ciudad != "sc"){
+        $ordenes = $ordenes->join('pdsperfiles','orden_requerimientos.pds_id', 'pdsperfiles.id')->where("pds_ciudad",$ciudad);
+    }
+
+    if($provincia != "sp"){
+        $ordenes = $ordenes->join('pdsperfiles','orden_requerimientos.pds_id', 'pdsperfiles.id')->where("pds_provincia",$provincia);
+    }
+}
+
+$ordenes = $ordenes->get();
+
+foreach($ordenes as $orden){
+
+}
+
+
+@endphp
+                        <div class="col-md-6 border py-5"><h2 class="my-5 text-primary">00:00</h2></div>
+                        <div class="col-md-6 border py-5"><h2 class="my-5 text-primary">00:00</h2></div>
+
+
                     </div>
                 </div>
             </div>
